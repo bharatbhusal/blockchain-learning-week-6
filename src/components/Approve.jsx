bc949b85a9d5511bc364b54c33e4723fe1176677
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-12-16 21:47:46 +0530

    approve

--- src/components/Approve.jsx
+++ src/components/Approve.jsx
@@ -1,29 +1,28 @@
 import { useContext, useEffect, useRef, useState } from "react";
-import { ethers } from "ethers"
+import { ethers, parseEther } from "ethers"
 import { toast } from "react-hot-toast";
 import { useAppContext } from "../context/useAppContext";
 
 const Approve = () => {
-
     const { withdrawContract, ethxContract, ethxBalance } = useAppContext();
     const approveStakeAmountRef = useRef();
 
     const approveToken = async (e) => {
-
         e.preventDefault();
-
-        const amount = approveStakeAmountRef.current.value.trim();
-
-        if (isNaN(amount) || amount <= 0)
-        {
-            toast.error("Please enter a valid positive number.");
-            return;
-        }
-
-        const amountToApprove = ethers.parseUnits(amount, 18).toString();
-
         try
         {
+            const amount = approveStakeAmountRef.current.value.trim();
+
+            if (isNaN(amount) || amount <= 0)
+                throw new Error("Please enter a valid positive number.");
+
+            const amountToApprove = ethers.parseUnits(amount, 18).toString();
+
+            approveStakeAmountRef.current.value = "";
+
+            if (parseEther(ethxBalance) <= amountToApprove)
+                throw new Error("Insufficent ETHx Balance")
+
             const approval = await ethxContract.approve(withdrawContract.target, amountToApprove)
 
             await toast.promise(approval.wait(),
@@ -33,36 +32,32 @@ const Approve = () => {
                     error: 'Approval failed 🤯'
                 });
 
-            approveStakeAmountRef.current.value = "";
 
         } catch (error)
         {
-            if (ethxContract == null)
-            {
-                toast.error("Connect To Wallet First")
-            } if (ethxBalance < approveStakeAmountRef)
-            {
-
-                toast.error("Insufficient ETHx Balance")
-            }
-            else
-            {
-                toast.error("Staking Failed");
-                console.error(error.message)
-            }
+            console.error(error.message)
+            toast.error(error.message)
         }
     };
 
     return (
-        <form onSubmit={approveToken} >
-            <label >Enter ETHx amount</label>
-            <input type="text" ref={approveStakeAmountRef} placeholder="0.0" />
-            <div >"AFTER APPROVAL ONLY YOU CAN UNSTAKE YOUR TOKEN"</div>
-            <button onClick={approveToken} type="submit" >
-                Approve
-            </button>
 
-        </form>
+        <>
+            {
+                ethxContract && withdrawContract ?
+                    <form onSubmit={approveToken} >
+                        <label >Enter ETHx amount</label>
+                        <input type="text" ref={approveStakeAmountRef} placeholder="0.0" />
+                        <div >"AFTER APPROVAL ONLY YOU CAN UNSTAKE YOUR TOKEN"</div>
+                        <button onClick={approveToken} type="submit" >
+                            Approve
+                        </button>
+
+                    </form>
+                    :
+                    <>EthxContract or WithdrawContract not avaiable</>
+            }
+        </>
     )
 }
 
