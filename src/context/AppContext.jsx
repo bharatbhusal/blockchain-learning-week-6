73ad5ed5f8e10b6f1a8a7b7541b28b4050819787
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-12-20 02:41:26 +0530

    final

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,20 +1,34 @@
-import { createContext, useState, useEffect } from 'react';
-import stakingAbi from '../ABI/stakingAbi.json';
-import withdrawAbi from '../ABI/withdrawAbi.json';
-import ethxAbi from '../ABI/ethxAbi.json';
-import { ethers, Contract } from 'ethers';
+import { createContext, useState, useEffect } from "react";
+import stakingAbi from "../ABI/stakingAbi.json";
+import withdrawAbi from "../ABI/withdrawAbi.json";
+import ethxAbi from "../ABI/ethxAbi.json";
+import { ethers, Contract } from "ethers";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+    // State to manage the Ethereum wallet signer
     const [signer, setSigner] = useState(null);
-    const [chainId, setChainId] = useState('chainId');
+
+    // State to manage the Ethereum network chain ID
+    const [chainId, setChainId] = useState("chainId");
+
+    // State to manage the user's Ethereum balance
     const [ethBalance, setEthBalance] = useState(null);
+
+    // State to manage the user's ETHx token balance
     const [ethxBalance, setEthxBalance] = useState(null);
+
+    // State to manage the Staking contract instance
     const [stakingContract, setStakingContract] = useState(null);
+
+    // State to manage the Withdraw contract instance
     const [withdrawContract, setWithdrawContract] = useState(null);
+
+    // State to manage the ETHx contract instance
     const [ethxContract, setEthxContract] = useState(null);
 
+    // Object containing the app's state values
     const state = {
         signer,
         setSigner,
@@ -32,58 +46,104 @@ export const AppContextProvider = ({ children }) => {
         setEthxContract,
     };
 
+    // Effect to listen for changes in the connected Ethereum account and network
     useEffect(() => {
-        // Event listeners for account and chain changes
-        const handleAccountChange = () => handleSignerChange(setSigner);
-        const handleChainChange = () => handleChainChange(setChainId);
+        // Add listeners for account and chain changes
+        window.ethereum.on("accountChanged", () => handleSignerChange(setSigner));
+        window.ethereum.on("chainChanged", () => handleChainChange(setChainId));
 
-        window.ethereum.on('accountChanged', handleAccountChange);
-        window.ethereum.on('chainChanged', handleChainChange);
-
+        // Cleanup: Remove listeners when the component unmounts
         return () => {
-            // Cleanup event listeners
-            window.ethereum.removeListener('accountChanged', handleAccountChange);
-            window.ethereum.removeListener('chainChanged', handleChainChange);
+            window.ethereum.removeListener("accountChanged", () =>
+                handleSignerChange(setSigner)
+            );
+            window.ethereum.removeListener("chainChanged", () =>
+                handleChainChange(setChainId)
+            );
         };
     }, []);
 
+    // Effect to create contract instances when the signer changes
     useEffect(() => {
-        // Create contract instances when signer is updated
-        const createContract = (abi, signer, address) => new Contract(address, abi, signer);
+        const createContract = (abi, signer, address) => {
+            return new Contract(address, abi, signer);
+        };
 
-        setStakingContract(createContract(stakingAbi, signer, '0xd0e400Ec6Ed9C803A9D9D3a602494393E806F823'));
-        setWithdrawContract(createContract(withdrawAbi, signer, '0x1048Eca024cB2Ba5eA720Ac057D804E95a809Fc8'));
-        setEthxContract(createContract(ethxAbi, signer, '0x3338eCd3ab3d3503c55c931d759fA6d78d287236'));
+        // Set contract instances based on the staking, withdraw, and ETHx contract addresses
+        setStakingContract(
+            createContract(stakingAbi, signer, "0xd0e400Ec6Ed9C803A9D9D3a602494393E806F823")
+        );
+        setWithdrawContract(
+            createContract(withdrawAbi, signer, "0x1048Eca024cB2Ba5eA720Ac057D804E95a809Fc8")
+        );
+        setEthxContract(
+            createContract(ethxAbi, signer, "0x3338eCd3ab3d3503c55c931d759fA6d78d287236")
+        );
     }, [signer]);
 
+    // Effect to update the user's Ethereum balance when the signer changes
     useEffect(() => {
-        // Fetch and update ETH balance when signer changes
         const returnEthBalance = async () => {
-            setEthBalance(ethers.formatEther(await signer.provider.getBalance(signer.address)));
+            // Fetch and format the Ethereum balance using the signer's provider
+            setEthBalance(
+                ethers.formatEther(await signer.provider.getBalance(signer.address))
+            );
         };
 
-        if (signer)
-        {
-            returnEthBalance();
-        }
+        // Call the function to update the Ethereum balance
+        returnEthBalance();
     }, [signer]);
 
+    // Effect to update the user's ETHx token balance when the ETHx contract changes
     useEffect(() => {
-        // Fetch and update ETHx balance when ethxContract changes
         const returnEthxBalance = async () => {
-            const contract = new Contract('0x3338eCd3ab3d3503c55c931d759fA6d78d287236', ethxAbi, signer);
-            setEthxBalance(ethers.formatEther(await contract.balanceOf(signer.address)));
+            // Create a contract instance for the ETHx token using the signer
+            const contract = new Contract(
+                "0x3338eCd3ab3d3503c55c931d759fA6d78d287236",
+                ethxAbi,
+                signer
+            );
+
+            console.log(signer.address);
+
+            // Fetch and format the ETHx balance using the contract instance
+            setEthxBalance(
+                ethers.formatEther(await contract.balanceOf(signer.address))
+            );
         };
 
-        if (signer)
-        {
-            returnEthxBalance();
-        }
-    }, [ethxContract, signer]);
+        // Check if the ETH balance is not null before updating ETHx balance
+        if (!ethBalance) returnEthxBalance();
+    }, [ethxContract]);
 
+    // Function to handle changes in the connected Ethereum account
+    const handleSignerChange = async (setSigner) => {
+        // Request the updated signer when the account changes
+        const newSigner = await window.ethereum.request({
+            method: "eth_requestAccounts",
+        });
+
+        // Set the new signer in the state
+        setSigner(newSigner);
+    };
+
+    // Function to handle changes in the Ethereum network chain ID
+    const handleChainChange = async (setChain) => {
+        // Request the updated chain ID when the network changes
+        let newChain = await window.ethereum.request({
+            method: "eth_chainId",
+        });
+
+        // Set the new chain ID in the state
+        setChain(newChain);
+    };
+
+    // Provide the app's state to the children components
     return (
         <AppContext.Provider value={state}>
             {children}
         </AppContext.Provider>
     );
 };
+
+// Include your comments
